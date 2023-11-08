5f70ec7323ab56f09c15e9d64745f8aae9c3bd18
Rolly Maulana Awangga <awangga@gmail.com>
2023-11-09 01:56:07 +0700

    OK

--- postfunction.js
+++ postfunction.js
@@ -1,6 +1,9 @@
 import { postWithToken } from "https://jscroot.github.io/api/croot.js";
 import {setInner,getValue} from "https://jscroot.github.io/element/croot.js";
 import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
+import { get } from "https://jscroot.github.io/api/croot.js";
+
+let urldevice = "https://api.wa.my.id/api/device/"+getCookie("login");
 
 export default function PostSignUp(){
     let target_url = "https://api.wa.my.id/api/signup";
@@ -20,4 +23,9 @@ export default function PostSignUp(){
 function responseData(result){
     setInner("ket","Yeay ini dia token kakak");
     setInner("nama",result.token);
+    get(urldevice,responseDevice);
+}
+
+function responseDevice(results){
+    setInner("judul","PAIR CODE: "+result.code);
 }
