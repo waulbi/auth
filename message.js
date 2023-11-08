d49c0689fafbf9a33e4aec44db91c628f83768c6
Rolly Maulana Awangga <awangga@gmail.com>
2023-11-09 02:12:50 +0700

    fungsi

--- message.js
+++ message.js
@@ -1,5 +1,5 @@
 import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
-
+import {setInner,getValue} from "https://jscroot.github.io/element/croot.js";
 
 let token=getCookie("login");
 if (token === ""){
