f986b4cd44028bc91746bcd7ac3aeb5eb3c08ea3
Rolly Maulana Awangga <awangga@gmail.com>
2023-11-09 01:05:22 +0700

    get cookie

--- index.js
+++ index.js
@@ -1,4 +1,3 @@
-import { setInner } from "https://jscroot.github.io/element/croot.js";
 import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
 import PostSignUp from "./postfunction.js";
 
--- postfunction.js
+++ postfunction.js
@@ -1,5 +1,6 @@
 import { postWithToken } from "https://jscroot.github.io/api/croot.js";
 import {setInner,getValue} from "https://jscroot.github.io/element/croot.js";
+import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
 
 export default function PostSignUp(){
     let target_url = "https://api.wa.my.id/api/signup";
