7a52f85a3bdf58ee6e1b5dea58fc35a84f579086
Rolly Maulana Awangga <awangga@gmail.com>
2023-11-09 01:02:47 +0700

    cuba

--- index.js
+++ index.js
@@ -1,10 +1,13 @@
 import { setInner } from "https://jscroot.github.io/element/croot.js";
 import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
+import PostSignUp from "./postfunction.js";
+
+
 
 let token=getCookie("login");
 if (token === ""){
     window.location.replace("../");
 }else{
-    setInner("nama",token);
+    window.PostSignUp = PostSignUp;
 }
 
---
+++ postfunction.js
@@ -0,0 +1,21 @@
+import { postWithToken } from "https://jscroot.github.io/api/croot.js";
+import {setInner,getValue} from "https://jscroot.github.io/element/croot.js";
+
+export default function PostSignUp(){
+    let target_url = "https://api.wa.my.id/api/signup";
+    let tokenkey = "token";
+    let tokenvalue = getCookie("login");
+    let datainjson = {
+        "url": getValue("url"),
+        "secret": getValue("secret")
+    }
+
+    postWithToken(target_url,tokenkey,tokenvalue,datainjson,responseData);
+
+}
+
+
+
+function responseData(result){
+    setInner("nama",result.token);
+}
