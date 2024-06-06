4ed70940d916bcb08bd5db253a4801b44ffa63cb
Rolly Maulana Awangga <awangga@gmail.com>
2024-06-06 09:48:29 +0700

    okeh dulu

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 import { getCookie } from "https://jscroot.github.io/cookie/croot.js";
+import { onClick } from "https://jscroot.github.io/element/croot.js";
 import PostSignUp from "./postfunction.js";
-
 
 
 let token=getCookie("login");
 if (token === ""){
     window.location.replace("../");
 }else{
-    window.PostSignUp = PostSignUp;
+    onClick('btn',PostSignUp);
 }
 
